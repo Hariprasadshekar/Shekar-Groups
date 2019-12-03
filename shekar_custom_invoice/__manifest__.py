2c608fe12a0ef7f581255fd333db2e6e16856896
Hariprasadshekar <shekargroups@prixgen.com>
2019-12-03 10:55:05 +0530

    Invoice testing

---
+++ shekar_custom_invoice/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Invoice report Template Shekar',
+    'version': '12.1.0.3',
+    'description': """This module consists, the customized invoice Templates""",
+    'category': 'Localization',
+    'author': 'Prixgen Tech Solutions Pvt Ltd.',
+    'company': 'Prixgen Tech Solutions Pvt Ltd.',
+    'website': 'https://www.prixgen.com',
+    'depends': ['account','l10n_in','web','base','product','custom_fields_shekar','customer_vendor_product_assets_number'],
+    'data': [
+          'views/tax_amount.xml',
+
+        'reports/invoice_report.xml',
+        'views/header_footer.xml',
+
+        
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
