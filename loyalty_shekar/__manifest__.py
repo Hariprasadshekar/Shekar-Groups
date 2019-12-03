7b81694e7341a18f155d085b9ba9150c58e3097d
Hariprasadshekar <shekargroups@prixgen.com>
2019-12-03 18:38:45 +0530

    Loyalty Shekar Testing

---
+++ loyalty_shekar/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Loyalty rewards report ',
+    'version': '12.6',
+    'category': 'Tools',
+    'summary': "This module consists, the customized Templates",
+    'depends': ['account','shekar_custom_invoice','sale_loyalty_ae'],
+    'website': 'http://www.prixgen.com',
+    'data': [
+             # 'views/custom_web.xml',
+             'reports/testing_rewards.xml',
+             'views/tax_amount.xml',
+             'views/header_footer.xml',
+
+             # 'views/rewards_report.xml',
+             # 'views/loyalty_rewards_report.xml',
+             ],
+    'auto_install': False,
+    'application': True,
+}
